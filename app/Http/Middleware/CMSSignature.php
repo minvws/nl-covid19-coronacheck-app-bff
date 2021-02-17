b5ce3db134da999970cc5bc5791d6fd00386fcce
Nick ten Cate <nick@nicktencate.com>
2021-02-17 14:02:28 +0100

    Add double-inline signature for easier debugging in dev

--- app/Http/Middleware/CMSSignature.php
+++ app/Http/Middleware/CMSSignature.php
@@ -24,8 +24,16 @@ class CMSSignature
         if(config('app.signature_format') == "inline") {
             return $response->setData(["signature" => $signature, "payload" => base64_encode($data)]);
         }
-        else {
+        else if(config('app.signature_format') == "inline-double") {
+            return $response->setData(
+                ["signature" => $signature, "payload" => base64_encode($data), "_payload" => $data]
+            );
+        }
+        else if(config('app.signature_format') == "header") {
             return $response->header('Signature', $signature);
+        }
+        else {
+            return $response;
         }
     }
 
