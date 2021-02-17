f3e7ba7c42349ee894cf864c6a777cb80c5b8df9
Nick ten Cate <nick@nicktencate.com>
2021-02-17 16:24:46 +0100

    Add missing exception include

--- app/Http/Controllers/HolderController.php
+++ app/Http/Controllers/HolderController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use \Exception;
 
 class HolderController extends BaseController
 {
