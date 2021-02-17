9f4711bb28980c4193d4b4334650bb21d0e96c69
Nick ten Cate <nick@nicktencate.com>
2021-02-17 03:57:07 +0100

    Make path to cdn files configurable

--- config/filesystems.php
+++ config/filesystems.php
@@ -4,7 +4,7 @@ return [
     'disks' => [
         'cdnfiles' => [
             'driver' => 'local',
-            'root'   => storage_path('../cdnfiles'),
+            'root'   => (str_starts_with(env('CDN_FILES_DIR'),'/') ? env('CDN_FILES_DIR') : realpath(dirname(__FILE__))."/../".env('CDN_FILES_DIR')),
         ],
     ],
 ];
