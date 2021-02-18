05a98f9d7c9181f7302f109e439be59ba67598a6
Nick ten Cate <nick@nicktencate.com>
2021-02-18 17:08:39 +0100

    Always allow BRB-TESTFLIGHT-Z2 / 1234 to create a signed test result

--- app/Services/OneTimeTestSignatureCheckService.php
+++ app/Services/OneTimeTestSignatureCheckService.php
@@ -12,6 +12,15 @@ class OneTimeTestSignatureCheckService
             return false;
         }
 
+        // This token can always get another signed test result
+        // BRB-TESTFLIGHT-Z2
+        if(config('app.env' != "production")
+            && $providerIdentifier == 'BRB'
+            && $unique = 'd3a368c584b02296974f69f368f04ba23a9e0149'
+        ) {
+            return true;
+        }
+
         $key = 'sign_test:' . hash_hmac('sha256',$providerIdentifier . ':'.$unique, "bananenbrood");
         $data = Redis::GET($key);
 
@@ -27,6 +36,7 @@ class OneTimeTestSignatureCheckService
 
         $key = 'sign_test:' . hash_hmac('sha256',$providerIdentifier . ':'.$unique, "bananenbrood");
         $data = Redis::INCR($key);
+
         Redis::EXPIRE($key, config('app.signed_test_hash_duration'));
 
         return ($data == 1);
